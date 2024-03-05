ce01994869f0bd73a72a98869b3e2c0156c11b04
Romashov Aleksey <aromashov@ozon.ru>
2024-03-05 14:43:00 +0300

    Add translator

--- ex4.py
+++ ex4.py
@@ -15,11 +15,33 @@ async def get_weather(city: int):
             except KeyError:
                 return 'Нет данных'
 
+async def get_translation(text: str, fromlanguage, tolanguage):
+    async with ClientSession() as session:
+        url = "https://word-translation-word-to-word.p.rapidapi.com/translatewordtoword"
+
+        data = {'word': text, 'fromlanguage': fromlanguage, 'tolanguage': tolanguage}
+
+        async with session.post(url, json=data) as response:
+            translate_json = await response.json()
+            print('!!!', translate_json)
+            try:
+                return translate_json['translatedText']
+            except KeyError:
+                return text
+
+
 
 async def handle(request):
-    city = request.rel_url.query['city']
-    weather = await get_weather(city)
-    result = {'city': city, 'weather': weather}
+    city_ru = request.rel_url.query['city']
+    city_en = await get_translation(city_ru, 'Russian', 'English')
+    print(city_en)
+
+    weather_en = await get_weather(city_en)
+    print('!!!', weather_en)
+    weather_ru = await get_translation(weather_en, 'English', 'Russian')
+    print('!!!', weather_ru)
+
+    result = {'city': city_ru, 'weather': weather_ru}
 
     return web.Response(text=json.dumps(result, ensure_ascii=False))
 
